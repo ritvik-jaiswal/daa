b41653a84b1b9ae5dcaf6d7ce3de5f5c6dafcc0e
Ritvik Jaiswal <56466425+ritvik-jaiswal@users.noreply.github.com>
2020-07-08 21:22:26 +0530

    Add files via upload

---
+++ b9queens.c
@@ -0,0 +1,73 @@
+#include<stdio.h>
+#include<math.h>
+#include<time.h>
+
+int a[30],count=0;
+
+int place(int pos)
+    {
+    int i;
+    for(i=1;i<pos;i++) 
+        {
+        if((a[i]==a[pos])||((abs(a[i]-a[pos])==abs(i-pos))))
+        return 0;
+        }
+    return 1;
+    }
+
+void print_sol(int n) 
+    {
+    int i,j; 
+    count++;
+    printf("\n\nSolution #%d:\n",count);
+    for(i=1;i<=n;i++)
+        {
+        for(j=1;j<=n;j++) 
+            {
+            if(a[i]==j)
+            printf("Q\t");
+            else
+            printf("*\t");
+            }
+        printf("\n");
+        }
+    }
+
+void queen(int n) 
+    {
+    int k=1;
+    a[k]=0;
+    while(k!=0) 
+        {
+        a[k]=a[k]+1;
+        while((a[k]<=n)&&!place(k))
+        a[k]++;
+        if(a[k]<=n) 
+            {
+            if(k==n)
+            print_sol(n);
+            else 
+                {
+                k++;
+                a[k]=0;
+                }
+            }
+        else
+        k--;
+        }
+    }
+
+void main()
+    {
+    clock_t starttime,endtime;
+    double clk;
+    int i,n;
+    printf("Enter the number of Queens\n");
+    scanf("%d",&n);
+    starttime=clock();
+    queen(n);
+    printf("\nTotal solutions=%d",count);
+    endtime=clock();
+    clk=(double)(endtime-starttime)/CLOCKS_PER_SEC;
+    printf("\nTotal time taken is %f sec",clk);
+    }
